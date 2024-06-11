0efbdf97b9df0ee35b8bbc1f493baa02785c1f0a
Mochi <antivirused29112003@gmail.com>
2024-06-11 10:01:36 +0700

    update main code to use recursion

--- src/main/java/com/navercorp/mathutil/core/MathUtility.java
+++ src/main/java/com/navercorp/mathutil/core/MathUtility.java
@@ -10,20 +10,18 @@ public class MathUtility {
     //Bài này chỉ làm từ 0 -> 20! vì 21! vượt (long)  
     public static long getFactorial(int n){
         
-        long product = 1; //tích kết quả phép nhân
+//        long product = 1; //tích kết quả phép nhân
         //update code again to see the green forever
         
         if(n < 0 || n > 20){
             throw new IllegalArgumentException("Invalid n. n must be between 0 ... 20");
         }
         
-        if(n == 0){
+        if(n == 0 || n == 1){
             return 1;
         }
         
-        for (int i = 1; i <= n; i++)
-            product *= i;
-        
-        return product;
+        return n * getFactorial(n-1);
+        //Recursion
     }
 }
